5ea3d63c44eca695b1bcb7ddc700b7fe92781147
ziord <ray25844@gmail.com>
2020-07-13 04:54:32 +0100

    updates

--- Splitty.hs
+++ Splitty.hs
@@ -2,20 +2,32 @@ module Splitty where
 import Data.List
 
 
-split::Char->[Char]->[[Char]]
-split ind []     = [] 
-split ind string@(x:xs)
-    | ind == x  = "": splitByIndex string indices indices
+clean::[String]->[String]
+clean = filter (/= "")
+
+
+splitByIndex::Char->String->[Int]->[Int]->[String]
+splitByIndex ind [] _ _ = []
+splitByIndex ind _ [] [] = []
+splitByIndex ind string@(x:xs) (c1:cxs) (c11:cxs11) = if t /= "" then trest else rest          
+    where   f indexList = if not $ null indexList then head indexList else length string
+            indices = elemIndices ind string
+            firstElemInd =  head indices
+            t = if c1 == firstElemInd then take firstElemInd string else ""
+            trest = t : drop (c1 +1) (take (f cxs11) string) : splitByIndex ind string cxs cxs11  
+            rest = drop (c1 +1) (take (f cxs11) string) : splitByIndex ind string cxs cxs11
+
+
+_split::Char->[Char]->[[Char]]
+_split ind []     = [] 
+_split ind string@(x:_)
+    | ind == x  = "": splitByIndex ind string indices indices
     | null indices = [string] 
-    | otherwise = splitByIndex string indices indices
+    | otherwise =  splitByIndex ind string indices indices
     where   indices = elemIndices ind string
-            splitByIndex::[Char]->[Int]->[Int]->[[Char]]
-            splitByIndex [] _ _ = []
-            splitByIndex _ [] [] = []
-            splitByIndex string@(x:xs) (c1:cxs) (c11:cxs11) = if t /= "" then trest else rest          
-                where   f indexList = if not (null indexList) then head indexList else length string
-                        firstElemInd =  head indices
-                        t = if c1 == firstElemInd then take firstElemInd string else ""
-                        trest = t : drop (c1 +1) (take (f cxs11) string) : splitByIndex string cxs cxs11  
-                        rest = drop (c1 +1) (take (f cxs11) string) : splitByIndex string cxs cxs11
+
+
+split::Char->[Char]->[[Char]]
+split ind str = if ind == ' ' then clean $ _split ind str else _split ind str
+            
 
